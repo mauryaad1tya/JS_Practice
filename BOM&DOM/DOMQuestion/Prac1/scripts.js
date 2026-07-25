c87a4f444b33f5567048d2541fd4d48eb96d643a
Aditya <mauryaaditya02003@gmail.com>
2026-07-25 20:19:49 +0530

    practice

--- BOM&DOM/DOMQuestion/Prac1/scripts.js
+++ BOM&DOM/DOMQuestion/Prac1/scripts.js
@@ -53,20 +53,176 @@
 // })
 
 
-let heading = document.getElementById("heading");
-let btn = document.getElementById("btn");
+// let heading = document.getElementById("heading");
+// let btn = document.getElementById("btn");
 
-let foods = [
-    "Pizza 🍕",
-    "Burger 🍔",
-    "Pasta 🍝",
-    "Biryani 🍛",
-    "Noodles 🍜",
-    "Ice Cream 🍨"
-];
+// let foods = [
+//     "Pizza 🍕",
+//     "Burger 🍔",
+//     "Pasta 🍝",
+//     "Biryani 🍛",
+//     "Noodles 🍜",
+//     "Ice Cream 🍨"
+// ];
 
-btn.addEventListener("click", function () {
-    let randomIndex = Math.floor(Math.random() * foods.length);
-    let food = foods[randomIndex]
-    heading.innerText = `The food for today is : ${food}`
-})
+// btn.addEventListener("click", function () {
+//     let randomIndex = Math.floor(Math.random() * foods.length);
+//     let food = foods[randomIndex]
+//     heading.innerText = `The food for today is : ${food}`
+// })
+
+
+// let employees = [
+//     {
+//         name: "Rahul",
+//         salary: 40000,
+//         department: "HR"
+//     },
+//     {
+//         name: "Aman",
+//         salary: 55000,
+//         department: "IT"
+//     },
+//     {
+//         name: "Priya",
+//         salary: 60000,
+//         department: "Finance"
+//     }
+// ];
+
+
+
+// for (let i = 0; i < employees.length; i++) {
+//     console.log(`${employees[i].name} works in ${employees[i].department} and earns salary ₹${employees[i].salary}`);
+
+// }
+// employees[0].salary = 45000
+
+// console.log(employees[0].salary);
+
+// employees.push({
+//     name: "Sneha",
+//     salary : 70000,
+//     department:"Marketing"
+// })
+
+// console.log(employees);
+
+
+// let movies = [];
+// movies.push({
+//     name: "Interstellar",
+//     rating: 10
+// },
+//     {
+//         name: "Inception",
+//         rating: 10
+//     },
+//     movies.push({
+//         name: "Avatar",
+//         rating: 9
+//     }
+// )
+
+// for (let i = 0; i < movies.length; i++) {
+//     console.log(`${movies[i].name} ⭐ ${movies[i].rating} `   );
+// }
+
+
+// let books = [];
+
+// books.push({
+//     title: "Atomic habits",
+//     author: "James Clear",
+//     price: 450
+// },
+//     {
+//         title: "Atomic habits",
+//         author: "James Clear",
+//         price: 450
+//     },
+//     {
+//         title: "Deep Work",
+//         author: "Cal Newport",
+//         price: 350
+//     },
+//     {
+//         title: "Clean Code",
+//         author: "Robert C. Martin",
+//         price: 650
+//     }
+// )
+
+// for (let i = 0; i < books.length; i++) {
+//    console.log(`${books[i].title} by ${books[i].author} costs ₹${books[i].price}`);
+
+// }
+
+
+// let cart = [];
+
+// cart.push(
+//     {
+//         name: "Laptop",
+//         price: 65000
+//     },
+//     { 
+//         price: 500
+//     },
+//     {
+//         name: "Keyboard",
+//         price: 1200
+//     }
+// )
+
+// for (let i = 0; i < cart.length; i++) {
+//     console.log(`${i+1}. ${cart[i].name} - ₹${cart[i].price}`);    
+// }
+
+// let phones = [
+//     {
+//         brand: "Samsung",
+//         price: 25000,
+//         stock: 10
+//     },
+//     {
+//         brand: "Apple",
+//         price: 85000,
+//         stock: 5
+//     },
+//     {
+//         brand: "OnePlus",
+//         price: 40000,
+//         stock: 8
+//     }
+// ];
+
+// for (let i = 0; i < phones.length; i++) {
+//     console.log(phones[i].brand);
+// }
+
+// phones[1].stock = 7
+
+// console.log(phones);
+
+// phones.push({
+//     brand: "Google",
+//     price: 50000,
+//     stock: 6
+// })
+
+
+
+// for (let i = 0; i < phones.length; i++) {
+//     let phone = phones[i]
+
+//     console.log(`${phone.brand} - ₹${phone.price} (${phone.stock}left)`);
+
+// }
+
+// let total = 0;
+// for (let i = 0; i < phones.length; i++) {
+//     let phone = phones[i]
+//     total += phone.price * phone.stock ;
+// }
+// console.log("Total inventory value = ",total);
--- BOM&DOM/DOMQuestion/todo/scripts.js
+++ BOM&DOM/DOMQuestion/todo/scripts.js
@@ -4,7 +4,38 @@ let output = document.getElementById("output");
 let message = document.getElementById("message");
 
 let todos = [];
+let editIndex = -1;
 
+function renderTodos() {
+
+    output.innerHTML = "";
+
+    for (let i = 0; i < todos.length; i++) {
+
+        let taskDiv = document.createElement("div");
+        taskDiv.style.margin = "10px";
+
+        let taskText = document.createElement("span");
+        taskText.innerText = todos[i].task;
+
+        let deleteBtn = document.createElement("button");
+        deleteBtn.innerText = "Delete";
+        deleteBtn.style.marginLeft = "10px";
+
+        deleteBtn.addEventListener("click", function () {
+            todos.splice(i, 1);
+            renderTodos();
+        });
+        let editBtn = document.createElement("button");
+        editBtn.innerText = "Edit";
+        editBtn.style.marginLeft ="10px"
+
+        taskDiv.appendChild(taskText);
+        taskDiv.appendChild(editBtn);
+        taskDiv.appendChild(deleteBtn);
+        output.appendChild(taskDiv);
+    }
+}
 addTodo.addEventListener("click", function () {
 
     if (todoInput.value.trim() === "") {
@@ -21,27 +52,9 @@ addTodo.addEventListener("click", function () {
 
     todos.push(todo);
 
-    output.innerHTML = "";
-
-    for (let i = 0; i < todos.length; i++) {
-
-        let taskDiv = document.createElement("div");
-
-        let taskText = document.createElement("span");
-        taskText.innerText = todos[i].task;
-
-        let deleteBtn = document.createElement("button");
-        deleteBtn.innerText = "Delete";
-
-        deleteBtn.addEventListener("click", function () {
-            taskDiv.remove();
-        });
-
-        taskDiv.appendChild(taskText);
-        taskDiv.appendChild(deleteBtn);
-
-        output.appendChild(taskDiv);
-    }
+    renderTodos();
 
     todoInput.value = "";
 });
+
+
