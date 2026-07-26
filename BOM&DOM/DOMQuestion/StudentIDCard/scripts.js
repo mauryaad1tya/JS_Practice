7a4adb2cca28622fe6d983bee18106f0e5a9959e
Aditya <mauryaaditya02003@gmail.com>
2026-07-27 00:02:45 +0530

    practice

--- BOM&DOM/DOMQuestion/StudentIDCard/scripts.js
+++ BOM&DOM/DOMQuestion/StudentIDCard/scripts.js
@@ -2,6 +2,9 @@ let change = document.getElementById("change")
 let box = document.getElementById("box")
 let heading = document.getElementById("heading")
 let mode = document.getElementById("mode")
+let name = document.getElementById("name")
+let course = document.getElementById("course")
+let city = document.getElementById("city")
 
 let colors = [
     "red",
@@ -17,11 +20,35 @@ let colors = [
 change.addEventListener("click", function () {
 
     let randomIndex = Math.floor(Math.random() * colors.length);
+    let randomIndex1 = Math.floor(Math.random() * colors.length);
     box.style.backgroundColor = colors[randomIndex];
-    heading.style.backgroundColor = colors[randomIndex];
+    let details = [heading, name, course, city];
 
+    for (let i = 0; i < details.length; i++) {
+        details[i].style.color = colors[randomIndex1];
+    }
 });
 
-mode.addEventListener("click", function(){
-    box.style.backgroundColor = "black"
-})
+let darkMode = false;
+
+mode.addEventListener("click", function () {
+
+    if (darkMode) {
+        box.style.backgroundColor = "white";
+        heading.style.color = "black";
+        name.style.color = "black";
+        course.style.color = "black";
+        city.style.color = "black";
+
+        darkMode = false;
+    } else {
+        box.style.backgroundColor = "black";
+        heading.style.color = "white";
+        name.style.color = "white";
+        course.style.color = "white";
+        city.style.color = "white";
+
+        darkMode = true;
+    }
+
+});
