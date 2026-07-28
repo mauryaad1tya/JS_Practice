fc8c4a14eb2d6fe377a928a3a2ce63cd6c0810e7
Aditya <mauryaaditya02003@gmail.com>
2026-07-28 23:37:03 +0530

    practice

---
+++ BOM&DOM/DOMQuestion/weatherChanger/scripts.js
@@ -0,0 +1,15 @@
+let weather = document.getElementById("weather")
+let icons = document.getElementById("icons")
+let city = document.getElementById("city")
+let temp = document.getElementById("temp")
+let change = document.getElementById("change")
+
+let weathers = [
+    
+]
+
+change.addEventListener("click", function(){
+    icons.innerText= "⛅";
+    temp.innerText= "21 C";
+    weather.innerText= "Cloudy";
+})
