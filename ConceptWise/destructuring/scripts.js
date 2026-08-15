843544e14b674c1c2ee8c7c4c6cb2548870e3a0b
Aditya <mauryaaditya02003@gmail.com>
2026-08-15 17:09:30 +0530

    practice

---
+++ ConceptWise/destructuring/scripts.js
@@ -0,0 +1,30 @@
+// let employee = {
+//     name: "Aditya",
+//     age: 23,
+//     salary: 60000,
+//     department: "IT"
+// }; 
+// let {name, age, salary, department} = employee;
+// console.log(name);
+
+// let product = {
+//         name: "Laptop",
+//         price: 65000,
+//         stock: 10
+// };
+
+// let { name, price, stock } = product;
+// console.log(name);
+// console.log(price);
+// console.log(stock);
+// console.log("Mahima");
+let product = {
+    name: "Laptop",
+    price: 65000,
+    stock: 10
+};
+
+let { name, price, stock } = product;
+console.log(name);
+console.log(price);
+console.log(stock);
