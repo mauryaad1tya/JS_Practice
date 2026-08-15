734d37b9c6dfb13cdf60df8e27efc6f14f5f91b1
Aditya <mauryaaditya02003@gmail.com>
2026-08-15 17:57:01 +0530

    Practice

--- ConceptWise/destructuring/scripts.js
+++ ConceptWise/destructuring/scripts.js
@@ -18,13 +18,76 @@
 // console.log(price);
 // console.log(stock);
 // console.log("Mahima");
-let product = {
-    name: "Laptop",
-    price: 65000,
-    stock: 10
-};
+// let product = {
+//     name: "Laptop",
+//     price: 65000,
+//     stock: 10
+// };
 
-let { name, price, stock } = product;
-console.log(name);
-console.log(price);
-console.log(stock);
+// let { name, price, stock } = product;
+// console.log(name);
+// console.log(price);
+// console.log(stock);
+
+// let employee = {
+//     name: "Rahul",
+//     salary: 50000,
+//     department: "IT"
+// };
+
+
+// let {name: employeeName, salary :employeeSalary, department: employeeDepartment} = employee
+
+// console.log(employeeName, employeeSalary, employeeDepartment);
+
+
+// let student = {
+//     name: "Rahul",
+//     age: 22
+// };
+
+// let {name: studentName, age : studentAge, course = "Not assigned"} = student
+
+// console.log(studentName);
+// console.log(studentAge);
+// console.log(course);
+
+// let numbers = [10, 20, 30];
+
+// let [first, second, third] = numbers;
+
+// console.log(first);
+// console.log(second);
+// console.log(third);
+
+// let fruits = ["Apple", "Mango", "Orange"];
+
+// let [mango, secondFruit, thirdFruit] = fruits
+// console.log(mango);
+// console.log(secondFruit);
+// console.log(thirdFruit);
+
+// let numbers = [10, 20, 30, 40, 50];
+
+// let [first, , third, ,fifth ] = numbers
+
+// console.log(first);
+// console.log(third);
+// console.log(fifth);
+
+// let colors = ["red", "blue"];
+// let [firstColor, secondColor, thirdColor = "green"] = colors
+
+// console.log(firstColor);
+// console.log(secondColor);
+// console.log(thirdColor);
+let numbers = [10, 20, 30, 40, 50];
+let [first, ...remaining] = numbers;
+
+console.log(first);
+console.log(remaining);
+
+let fruits = ["Apple", "Mango", "Orange", "Banana", "Grapes"];
+let [firstFruit, ...rest] = fruits
+console.log(firstFruit);
+console.log(rest);
---
+++ ConceptWise/spread/scripts.js
@@ -0,0 +1,53 @@
+// let numbers = [10, 20, 30];
+
+// let newNumbers = [...numbers];
+
+// console.log(newNumbers);
+
+// let fruits = ["Apple", "Mango", "Orange"];
+// let newFruits = [...fruits, "Banana"];
+
+
+// console.log(newFruits);
+// console.log(fruits);
+
+// let fruits = ["Apple", "Mango"];
+// let vegetables = ["Carrot", "Potato"];
+
+// let food = [...fruits, ...vegetables];
+
+// console.log(food);
+// let numbers = [10, 20, 30];
+
+// let newNumbers = [...numbers, "40"]
+// console.log(newNumbers);
+
+// let frontend = ["HTML", "CSS", "JavaScript"];
+// let backend = ["Node.js", "Express", "MongoDB"];
+
+// let techStack = [...frontend, ...backend]
+
+// console.log(techStack);
+
+// let product = {
+//     name: "Laptop",
+//     price: 65000
+// };
+
+// let updatedProduct = {...product, stock:10}
+// console.log(updatedProduct);
+
+// let user = {
+//     name: "Rahul",
+//     age: 22,
+//     city: "Delhi"
+// };
+
+// let updatedUser = {...user, age : 23, role : "Developer"}
+
+// console.log(updatedUser);
+
+let numbers = [10, 20, 30];
+let updatedNumbers = [...numbers, 40,50]
+
+console.log(updatedNumbers);
