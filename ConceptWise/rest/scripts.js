777b13cdf03ae49b6b9001ce2ca53355c89404bb
Aditya <mauryaaditya02003@gmail.com>
2026-08-20 21:20:40 +0530

    practice

--- ConceptWise/rest/scripts.js
+++ ConceptWise/rest/scripts.js
@@ -4,10 +4,10 @@
 // add(10,20,30,40)
 
 
-function addNumbers(...numbers){
-    return numbers.reduce(function(sum, number){
-        return sum+number
-    })
-}
-console.log(addNumbers(10,50,80,90,60));
+// function addNumbers(...numbers){
+//     return numbers.reduce(function(sum, number){
+//         return sum+number
+//     })
+// }
+// console.log(addNumbers(10,50,80,90,60));
 
