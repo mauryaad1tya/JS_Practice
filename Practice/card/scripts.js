e3be8fd858048742768cd00f0212cde5878afa6c
Aditya <mauryaaditya02003@gmail.com>
2026-08-26 12:00:00 +0530

    practice

---
+++ Practice/card/scripts.js
@@ -0,0 +1,24 @@
+let card = document.getElementById("card");
+
+
+let student = {
+    name: "Aditya",
+    course: "JavaScript",
+    marks: 85,
+    image: "https://via.placeholder.com/150"
+};
+
+function showStudent(student) {
+
+    card.innerHTML = `
+        <img src="${student.image}" alt="Student Image">
+
+        <h2>${student.name}</h2>
+
+        <p>Course: ${student.course}</p>
+
+        <p>Marks: ${student.marks}</p>
+    `;
+}
+
+showStudent(student)
